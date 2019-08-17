6692c8c3e339130f18ca0e0a24f7e8f4933c3f4e
Mohamed Hisham <Mohamed00Hisham@Gmail.com>
2019-08-17 17:11:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,43 +1,58 @@
 #!/usr/bin/env python2
 """
-    Project settings & shared variables.
+    Project settings & configuration.
+    These values DO NOT CHANGE during runtime.
 
 """
 
+# colors
+GREEN = color(155, 255, 157)
+RED   = color(255, 155, 157)
+BLUE  = color(155, 157, 255)
+BLACK = color(0, 0, 0)
+
 # environment settings
-WIDTH, HEIGHT = 1200, 600           # window width & height
-BARRIER_COLOR = color(0, 0, 0)      # barrier color
-BACKGROUND = color(255, 255, 184)   # background color
+WIDTH, HEIGHT = 1200, 600          # window width & height
+BACKGROUND = color(245, 250, 250)    # background color
 
-# shared global variables
-BARRIERS   = []       # keeps track of the barriers
-POPULATION = []       # keeps track of the population object
-FRAMES     = []       # stores the ant sprite animation frames
+# text settings
+TEXT_COLOR = BLACK    # text color
+TEXT_SIZE    = 15     # text size
 
-START_TIME = 0        # used for calculating the elapsed time
+# target settings
+TARGET            = PVector(WIDTH/2, 40) # position of the target
+TARGET_COLOR      = BLACK                # target color
+TARGET_HUE_COLOR  = color(50, 100)       # hue color (around the target)
+TARGET_RADIUS     = 20                   # the radius of the target
+TARGET_HUE_RADIUS = TARGET_RADIUS + 20   # the radius of the hue around the target
 
-BARRIER_START = PVector(0, 0) # mark the start of the barrier
-BARRIER_END   = PVector(0, 0) # mark the end of the barrier
-MINIMUM_BARRIER_LENGTH = 30   # minimum barrier length
+# shadow settings
+SHADOW_RADIUS = 30    # shadow radius around the best/worst individuals
 
-# sprite animation files
+# barrier settings
+BARRIER_STROKE_WEIGHT  = 2
+BARRIER_COLOR          = BLACK     # the color of the barier object
+MINIMUM_BARRIER_LENGTH = 30        # minimum barrier length
+
+# sprite animation settings
 SPRITE_SHEET       = "animation/ant.png"    # the sprite sheet
 SPRITE_SHEET_DATA  = "animation/ant.json"   # sprite sheet json data
+FRAME_WIDTH        = 20                     # sprite animation frame width
+FRAME_HEIGHT       = 26                     # sprite animation frame height
 
-# sprite animation properties
-FRAME_WIDTH = 20     # sprite animation frame width
-FRAME_HEIGHT = 26    # sprite animation frame height
+# dot animation settings
+DOT_RADIUS = 7     # check the ant show function
 
-# initial positions
-TARGET            = PVector(WIDTH/2, 40)      # position of the target
+# initial ants values
 INIT_POSITION     = PVector(WIDTH//2, HEIGHT) # initial ants position
 INIT_VELOSITY     = PVector(0, 0)             # initial ants velosity
 INIT_ACCELERATION = PVector(0, 0)             # initial ants acceleration
 
 # genetic algorithm settings
-POP_SIZE           = 50          # population size
-LIFE_SPAN          = 250          # ants life span
-INIT_MUTATION_RATE = 0.01         # initial mutation rate for each ant
+POP_SIZE                     = 50       # population size
+LIFE_SPAN                    = 250      # ants life span
+INIT_MUTATION_RATE           = 0.01     # initial mutation rate for each ant
+TOURNAMENT_SAMPLE_PERCENTAGE = 50       # tournament selection sample percentage
 
 # physics constants
 # DO NOT CHANGE THESE VALUES, AS IT MIGHT BREAK THE COLLISION DETECTION IN THE BARRIERS
